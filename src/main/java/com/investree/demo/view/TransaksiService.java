23bc5532f967d2652113fdc027573f7d7e0184a1
richardsinaga261@gmail.com <richardsinaga261@gmail.com>
2022-10-02 20:47:34 +0700

    lima

--- src/main/java/com/investree/demo/view/TransaksiService.java
+++ src/main/java/com/investree/demo/view/TransaksiService.java
@@ -1,9 +1,12 @@
 package com.investree.demo.view;
 
+import java.util.List;
 import java.util.Map;
 
 import com.investree.demo.dto.TransaksiCreateRequestDTO;
 import com.investree.demo.model.Transaksi;
+
+import org.springframework.data.domain.Page;
 
 public interface TransaksiService {
     public Page<Transaksi> list(int page, int size,String status);
